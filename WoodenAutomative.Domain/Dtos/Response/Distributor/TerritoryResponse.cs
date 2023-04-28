e8a698dc100ab2fb991c7511a2fb4cadd6acb251
Jigar Chaudhari <jigar.xtremethoughts@outlook.com>
2023-04-28 20:55:39 +0530

    commit done changes add Users details in Distribute admin and aspnet users TB

---
+++ WoodenAutomative.Domain/Dtos/Response/Distributor/TerritoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoodenAutomative.Domain.Dtos.Response.Distributor
+{
+    public class TerritoryResponse
+    {
+        public int? TerritoryID { get; set; }
+        public string TerritoryName { get; set; }
+    }
+}
